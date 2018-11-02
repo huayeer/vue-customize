539006df8cfee0b0ebed2ae09a2b46033a31f73f
quwei3 <quwei3@wanda.cn>
2018-11-02 14:54:42 +0800

    submit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class=all>
+    <tab :line-width=2 active-color='#000' v-model="index">
+      <tab-item  :selected="demo2 === item" v-for="(item, index) in list2" @click="demo2 = item" :key="index">{{item}}</tab-item>
+    </tab>
+    <swiper v-model="index" height="100px" :show-dots="false">
+      <swiper-item v-for="(item, index) in list2" :key="index">
+        <div>{{item}} Container</div>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+<script>
+import { Tab, TabItem, Swiper, SwiperItem } from 'vux'
+const list = () => ['精选', '美食', '电影', '酒店', '外卖', '电影', '酒店', '外卖', '电影', '酒店', '外卖']
+
+export default {
+  components: {
+    Tab,
+    TabItem,
+    Swiper,
+    SwiperItem
+  },
+  data () {
+    return {
+      list2: list(),
+      demo2: '精选',
+      
+      index: 0,
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="less" scoped>
+	.vux-tab .vux-tab-item{
+	  background:none;
+	  color:#000 !important
+	}
+</style>
+<style>
+.myClass .scrollable .vux-tab-ink-bar {
+  bottom: 28px
+}
+</style>
---
+++ src/pages/home/Vuxtab.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class=all>
+    <tab ref="tab" active-color='#000' v-model="index" :box-width="boxWidth">
+      <tab-item  :selected="demo === item" v-for="(item, index) in list" @click="demo = item" :key="index">{{item}}</tab-item>
+    </tab>
+    <swiper v-model="index" height="100px" :show-dots="false">
+      <swiper-item v-for="(item, index) in list" :key="index">
+        <div>{{item}} Container</div>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+<script>
+import { Tab, TabItem, Swiper, SwiperItem } from 'vux'
+const list = () => ['智能精选', '美食', '电影', '酒店', '外卖', '电影', '酒店', '外卖', '电影', '酒店', '外卖']
+
+export default {
+  components: {
+    Tab,
+    TabItem,
+    Swiper,
+    SwiperItem
+  },
+  data () {
+    return {
+      list: list(),
+      demo: '精选',
+      boxWidth: 0,
+      index: 0
+    }
+  },
+  mounted: function () {
+    this.boxWidth = this.$refs.tab.$el.getBoundingClientRect().width
+  }
+}
+</script>
+<style>
+  .all{
+    padding:0 20px;
+  }
+</style>
